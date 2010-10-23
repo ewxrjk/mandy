5f2021d488cfe0ab633d6c8d7ec6ea54b4b2b258
Richard Kettlewell <rjk@greenend.org.uk>
2010-10-23 21:11:55 +0100

    WIP.  drag, report location in label

--- mand.c
+++ mand.c
@@ -29,6 +29,7 @@ static int calc(double cx, double cy) {
 
 static void mand(double x, double y, double size, int xpixels, int ypixels,
 		 int *results) {
+  // TODO take advantage of multiple cores!
   for(int py = 0; py < ypixels; ++py) {
     for(int px = 0; px < xpixels; ++px) {
       *results++ = calc(x + px * size / xpixels,
@@ -54,6 +55,14 @@ static void fatal(int errno_value, const char *fmt, ...) {
 // x, y are the centre
 // size is the distance from the centre to the nearest edge
 static double x = 0.0, y = 0.0, size = 2.0;
+static GtkLabel *report_label;
+
+static void report(void) {
+  char buffer[128];
+  snprintf(buffer, sizeof buffer,
+	   "%g x %g radius %g", x, y, size);
+  gtk_label_set_text(report_label, buffer);
+}
 
 static int *recompute(int w, int h) {
   int *iters = malloc(w * h * sizeof(int));
@@ -89,7 +98,8 @@ static void redraw(GtkWidget *widget,
   gdk_drawable_get_size(widget->window, &w, &h);
 
   if(w != lastw || h != lasth || force) {
-    // The drawable size has changed, redraw it
+    // Regenerate the pixel data, either because the size has changed
+    // or because we're looking at something different.
     lastw = w;
     lasth = h;
     int *iters = recompute(w, h);
@@ -123,11 +133,16 @@ static gboolean exposed(GtkWidget *widget,
   return TRUE;
 }
 
+
+static gboolean dragging;
+static double dragx, dragy;
+
 static gboolean button_pressed(GtkWidget *widget,
 			       GdkEventButton *event,
 			       gpointer __attribute__((unused)) data) {
+  // Middle button zooms
   if(event->type == GDK_BUTTON_PRESS
-     && event->button == 1
+     && event->button == 2
      && event->state == 0) {
     gint w, h;
     gdk_drawable_get_size(widget->window, &w, &h);
@@ -146,10 +161,50 @@ static gboolean button_pressed(GtkWidget *widget,
     x = xleft + event->x * xsize / w;
     y = ybottom + (h - 1 - event->y) * ysize / h;
     size = size / 1.414;
+    report();
     redraw(widget, TRUE);
     return TRUE;
   }
+  // Left button drags
+  if(event->type == GDK_BUTTON_PRESS
+     && event->button == 1
+     && event->state == 0) {
+    dragging = TRUE;
+    dragx = event->x;
+    dragy = event->y;
+    return TRUE;
+  }
+  if(event->type == GDK_BUTTON_RELEASE
+     && event->button == 1) {
+    dragging = FALSE;
+    return TRUE;
+  }
   return FALSE;
+}
+
+static gboolean pointer_moved(GtkWidget *widget,
+			      GdkEventMotion *event,
+			      gpointer __attribute__((unused)) user_data) {
+  if(!dragging)
+    return FALSE;
+  double deltax = event->x - dragx;
+  double deltay = event->y - dragy;
+  if(!(deltax == 0 && deltay == 0)) {
+    dragx = event->x;
+    dragy = event->y;
+    gint w, h;
+    gdk_drawable_get_size(widget->window, &w, &h);
+    if(w > h) {
+      x -= deltax * size * 2 / h;
+      y += deltay * size * 2 / h;
+    } else {
+      x -= deltax * size * 2 / w;
+      y += deltay * size * 2 / w;
+    }
+    report();
+    redraw(widget, TRUE);
+  }
+  return TRUE;
 }
 
 static gboolean deleted(GtkWidget __attribute__((unused)) *widget,
@@ -175,11 +230,18 @@ int main(int argc, char **argv) {
   GtkWidget *da = gtk_drawing_area_new();
   gtk_widget_set_size_request(da, 256, 256);
   g_signal_connect(da, "expose-event", G_CALLBACK(exposed), NULL);
-  gtk_widget_add_events(da, GDK_BUTTON_PRESS_MASK);
+  gtk_widget_add_events(da,
+			GDK_BUTTON_PRESS_MASK
+			|GDK_BUTTON_RELEASE_MASK
+			|GDK_POINTER_MOTION_MASK);
   g_signal_connect(da, "button-press-event", G_CALLBACK(button_pressed), NULL);
+  g_signal_connect(da, "button-release-event", G_CALLBACK(button_pressed), NULL);
+  g_signal_connect(da, "motion-notify-event", G_CALLBACK(pointer_moved), NULL);
 
   // A label to say what we're doing
   GtkWidget *label = gtk_label_new("spong");
+  report_label = (GtkLabel *)label;
+  report();
 
   // Pack it together vertically
   GtkWidget *vbox = gtk_vbox_new(FALSE, 0);
