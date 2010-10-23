a7fef9e3ac38b8129de44fcb72373905ba1dda80
Richard Kettlewell <rjk@greenend.org.uk>
2010-10-23 22:37:23 +0100

    WIP more interesting colors

--- mand.c
+++ mand.c
@@ -99,8 +99,8 @@ static void destroy_threads(void) {
   if((rc = pthread_mutex_lock(&lock)))
     fatal(rc, "pthread_mutex_lock");
   quit_threads = 1;
-    if((rc = pthread_cond_broadcast(&cond)))
-      fatal(rc, "pthread_cond_broadcast");
+  if((rc = pthread_cond_broadcast(&cond)))
+    fatal(rc, "pthread_cond_broadcast");
   if((rc = pthread_mutex_unlock(&lock)))
     fatal(rc, "pthread_mutex_unlock");
   for(int n = 0; n < ncores; ++n) {
@@ -173,9 +173,9 @@ static struct {
 static void init_colors(void) {
   // The complement is colorful
   for(int n = 0; n < MAXITER; ++n) {
-    colors[n].r = (255 * (MAXITER - n)) / MAXITER;
-    colors[n].g = 255 - (cos((double)n / MAXITER) + 1.0) * 127;
-    colors[n].b = (255 * n) / MAXITER;
+    colors[n].r = (cos(2 * M_PI * (double)n / MAXITER) + 1.0) * 127;
+    colors[n].g = 255 - (cos(4 * M_PI * (double)n / MAXITER) + 1.0) * 127;
+    colors[n].g = 255 - (cos(8 * M_PI * (double)n / MAXITER) + 1.0) * 127;
   }
   // The set itself is black
   colors[MAXITER].r = colors[MAXITER].g = colors[MAXITER].b = 0;
