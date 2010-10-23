81e8e94db7a197c9fbc831d6626b87f2184b5d85
Richard Kettlewell <rjk@greenend.org.uk>
2010-10-23 21:55:15 +0100

    WIP.  distribute work across all available CPUs.

--- mand.c
+++ mand.c
@@ -4,11 +4,24 @@
 #include <string.h>
 #include <errno.h>
 #include <math.h>
+#include <unistd.h>
+#include <pthread.h>
 #include <glib.h>
 #include <gtk/gtk.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
 
 #define MAXITER 255
+
+static int ncores;
+static struct threadinfo {
+  pthread_t id;
+  double x, y, size;
+  int xpixels, ypixels;
+  int *results;
+} *threads;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static gboolean quit_threads;
 
 static int calc(double cx, double cy) {
   // let c = cx + icy
@@ -27,17 +40,6 @@ static int calc(double cx, double cy) {
   return iterations;
 }
 
-static void mand(double x, double y, double size, int xpixels, int ypixels,
-		 int *results) {
-  // TODO take advantage of multiple cores!
-  for(int py = 0; py < ypixels; ++py) {
-    for(int px = 0; px < xpixels; ++px) {
-      *results++ = calc(x + px * size / xpixels,
-			y + py * size / xpixels);
-    }
-  }
-}
-
 static void fatal(int errno_value, const char *fmt, ...) {
   va_list ap;
 
@@ -50,6 +52,96 @@ static void fatal(int errno_value, const char *fmt, ...) {
   else
     fputc('\n', stderr);
   exit(1);
+}
+
+static void *worker(void *arg) {
+  struct threadinfo *me = arg;
+  int rc;
+
+  if((rc = pthread_mutex_lock(&lock)))
+    fatal(rc, "pthread_mutex_lock");
+  while(!quit_threads) {
+    if(!me->results) {
+      if((rc = pthread_cond_wait(&cond, &lock)))
+	fatal(rc, "pthread_cond_wait");
+      continue;
+    }
+    int *results = me->results;
+    if((rc = pthread_mutex_unlock(&lock)))
+      fatal(rc, "pthread_mutex_unlock");
+    for(int py = 0; py < me->ypixels; ++py)
+      for(int px = 0; px < me->xpixels; ++px)
+	*results++ = calc(me->x + px * me->size / me->xpixels,
+			  me->y + py * me->size / me->xpixels);
+    if((rc = pthread_mutex_lock(&lock)))
+      fatal(rc, "pthread_mutex_lock");
+    me->results = 0;
+    if((rc = pthread_cond_broadcast(&cond)))
+      fatal(rc, "pthread_cond_broadcast");
+  }
+  if((rc = pthread_mutex_unlock(&lock)))
+    fatal(rc, "pthread_mutex_unlock");
+  return NULL;
+}
+
+static void init_threads(void) {
+  ncores = sysconf(_SC_NPROCESSORS_ONLN);
+  threads = malloc(ncores * sizeof *threads);
+  memset(threads, 0, ncores * sizeof *threads);
+  int rc;
+  for(int n = 0; n < ncores; ++n)
+    if((rc = pthread_create(&threads[n].id, NULL, worker, &threads[n])))
+      fatal(rc, "pthread_create");
+}
+
+static void destroy_threads(void) {
+  int rc;
+  if((rc = pthread_mutex_lock(&lock)))
+    fatal(rc, "pthread_mutex_lock");
+  quit_threads = 1;
+    if((rc = pthread_cond_broadcast(&cond)))
+      fatal(rc, "pthread_cond_broadcast");
+  if((rc = pthread_mutex_unlock(&lock)))
+    fatal(rc, "pthread_mutex_unlock");
+  for(int n = 0; n < ncores; ++n) {
+    if((rc = pthread_join(threads[n].id, NULL)))
+      fatal(rc, "pthread_join");
+  }
+}
+
+// Compute values for [x,x+size) x [y, y+size) in xpixels * ypixels
+static void mand(double x, double y, double size, int xpixels, int ypixels,
+		 int *results) {
+  int rc;
+  if((rc = pthread_mutex_lock(&lock)))
+    fatal(rc, "pthread_mutex_lock");
+  // Fill in the per-thread requests
+  int ychunk = ypixels / ncores;
+  int extra = ypixels % ncores != 0;
+  for(int n = 0; n < ncores; ++n) {
+    threads[n].x = x;
+    threads[n].y = y + n * ychunk * size / xpixels;
+    threads[n].size = size;
+    threads[n].xpixels = xpixels;
+    threads[n].ypixels = ychunk + (n == ncores - 1 && extra);
+    threads[n].results = results + xpixels * n * ychunk;
+  }
+  // Set them going
+  if((rc = pthread_cond_broadcast(&cond)))
+    fatal(rc, "pthread_cond_broadcast");
+  // Wait for them to finish
+  for(;;) {
+    int finished = 0;
+    for(int n = 0; n < ncores; ++n)
+      if(threads[n].results == NULL)
+	++finished;
+    if(finished >= ncores)
+      break;
+    if((rc = pthread_cond_wait(&cond, &lock)))
+      fatal(rc, "pthread_cond_wait");
+  }
+  if((rc = pthread_mutex_unlock(&lock)))
+    fatal(rc, "pthread_mutex_unlock");
 }
 
 // x, y are the centre
@@ -210,6 +302,7 @@ static gboolean pointer_moved(GtkWidget *widget,
 static gboolean deleted(GtkWidget __attribute__((unused)) *widget,
 			GdkEvent __attribute__((unused)) *event,
 			gpointer __attribute__((unused)) data) {
+  destroy_threads();
   exit(0);
 }
 
@@ -219,8 +312,11 @@ int main(int argc, char **argv) {
   if(!gtk_init_check(&argc, &argv))
     fatal(0, "gtk_init_check failed");
 
+  // Bits of infrastructure
+  init_threads();
   init_colors();
 
+  // The top level window
   GtkWidget *toplevel = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title((GtkWindow *)toplevel, "mand");
   g_signal_connect(G_OBJECT(toplevel), "delete-event",
