5562b6a7034df2360101cbfa4f24bbad6a1271b5
Richard Kettlewell <rjk@greenend.org.uk>
2010-10-24 11:14:36 +0100

    blue component was not set!

--- colors.c
+++ colors.c
@@ -9,7 +9,7 @@ void init_colors(void) {
   for(int n = 0; n < MAXITER; ++n) {
     colors[n].r = (cos(2 * M_PI * (double)n / MAXITER) + 1.0) * 127;
     colors[n].g = 255 - (cos(4 * M_PI * (double)n / MAXITER) + 1.0) * 127;
-    colors[n].g = 255 - (cos(8 * M_PI * (double)n / MAXITER) + 1.0) * 127;
+    colors[n].b = 255 - (cos(8 * M_PI * (double)n / MAXITER) + 1.0) * 127;
   }
   // The set itself is black
   colors[MAXITER].r = colors[MAXITER].g = colors[MAXITER].b = 0;
