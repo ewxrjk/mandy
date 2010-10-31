ddd32cf1b8bb0cd98488f05b897d6b1061d5c73b
Richard Kettlewell <rjk@greenend.org.uk>
2010-10-31 22:05:52 +0000

    JuliaJob implementation

---
+++ lib/JuliaJob.h
@@ -0,0 +1,49 @@
+/* Copyright © 2010 Richard Kettlewell.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef JULIAJOB_H
+#define JULIAJOB_H
+
+#include "FractalJob.h"
+
+class JuliaJob: public FractalJob {
+  double cx, cy;
+public:
+  JuliaJob(double cx_, double cy_): cx(cx_), cy(cy_) {
+  }
+
+  // Do the computation (called in background thread)
+  void work();
+};
+
+class JuliaJobFactory: public FractalJobFactory {
+public:
+  JuliaJobFactory(): cx(0), cy(0) {
+  }
+  double cx, cy;
+  FractalJob *create() const;
+};
+
+#endif /* JULIAJOB_H */
+
+/*
+Local Variables:
+mode:c++
+c-basic-offset:2
+comment-column:40
+fill-column:79
+indent-tabs-mode:nil
+End:
+*/
