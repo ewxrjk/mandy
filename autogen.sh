52466218f35baeda0ffe668cb4d7f4782515c36b
Richard Kettlewell <rjk@greenend.org.uk>
2013-10-26 11:06:37 +0100

    Use autoreconf

--- autogen.sh
+++ autogen.sh
@@ -17,10 +17,4 @@ set -e
 srcdir=$(dirname $0)
 here=$(pwd)
 cd $srcdir
-mkdir -p config.aux
-aclocal
-#libtoolize
-autoconf
-autoheader
-automake -a || true		# for INSTALL
-automake --foreign -a
+autoreconf -is
