4057b8b347f3816dd42bff79d8737709b7142457
Richard Kettlewell <rjk@greenend.org.uk>
2021-11-27 17:29:22 +0000

    movie: select width, height, precision

--- gtkui/Movie.cc
+++ gtkui/Movie.cc
@@ -34,56 +34,23 @@ class MovieWindow;
 class MovieControls: public ControlContainer {
 public:
   MovieWindow *m_window;
-  arith_t m_x = 0, m_y = 0, m_radius = 2;
-  arith_type m_arith = ARITH_DEFAULT;
-  int m_maxiters = 255, m_seconds = 10, m_fps = 25;
-  int m_bitrate = 8 * 1024 * 1024;
-  std::string m_codec = "libx264";
-  std::string m_ffmpeg = ffmpegDefault();
-  std::string m_path = "mandy.mp4";
 
   RealControl m_x_control, m_y_control, m_radius_control;
   IntegerControl m_maxiters_control, m_seconds_control, m_fps_control,
-      m_bitrate_control;
+      m_bitrate_control, m_width_control, m_height_control;
   StringControl m_codec_control;
   FileSelectionControl m_ffmpeg_control;
   StringControl m_path_control; // TODO use a file chooser
+  DropDownControl m_arith_control;
 
-  MovieControls(MovieWindow *window):
-      m_window(window),
-      m_x_control(this, &m_x, -arith_traits<arith_t>::maximum(),
-                  arith_traits<arith_t>::maximum()),
-      m_y_control(this, &m_y, -arith_traits<arith_t>::maximum(),
-                  arith_traits<arith_t>::maximum()),
-      m_radius_control(this, &m_radius, 0, arith_traits<arith_t>::maximum()),
-      m_maxiters_control(this, &m_maxiters, 1, INT_MAX - 1),
-      m_seconds_control(this, &m_seconds, 1, INT_MAX - 1),
-      m_fps_control(this, &m_fps, 1, INT_MAX - 1),
-      m_bitrate_control(this, &m_bitrate, 1, INT_MAX - 1),
-      m_codec_control(this, &m_codec),
-      m_ffmpeg_control(this, &m_ffmpeg, "Select Encoder"),
-      m_path_control(this, &m_path) {
-
-    m_x_control.Attach(0, 0, "X center");
-    m_y_control.Attach(0, 1, "Y center");
-    m_radius_control.Attach(0, 2, "Radius");
-    m_maxiters_control.Attach(0, 3, "Iterations");
-
-    m_seconds_control.Attach(1, 0, "Length (s)");
-    m_fps_control.Attach(1, 1, "Frames/s");
-    m_bitrate_control.Attach(1, 2, "Bits/s");
-    m_codec_control.Attach(1, 3, "Codec");
-
-    m_ffmpeg_control.Attach(0, 4, "Encoder", 2);
-    m_path_control.Attach(0, 5, "Filename", 2);
-  }
+  MovieControls(MovieWindow *window);
 
   void controlChanged(Control *);
 };
 
 class MovieWindow: public Gtk::Window {
 public:
-  MovieControls controls;
+  MovieControls *controls = nullptr;
   Gtk::Frame frame;
   Gtk::HBox buttons;
   Gtk::Button render, cancel;
@@ -92,6 +59,17 @@ public:
 
   bool working;
   threadid_t thread;
+
+  // Parameters
+  arith_t m_x = 0, m_y = 0, m_radius = 2;
+  arith_type m_arith = ARITH_DEFAULT;
+  int m_maxiters = 255, m_seconds = 10, m_fps = 25;
+  int m_bitrate = 8 * 1024 * 1024;
+  int m_width = 1280, m_height = 720;
+  std::string m_codec = "libx264";
+  std::string m_ffmpeg = ffmpegDefault();
+  std::string m_path = "mandy.mp4";
+  std::string m_arith_string = arith_names[ARITH_DEFAULT];
 
   // Progress report form the worker
   class Progress: public Job {
@@ -117,10 +95,15 @@ public:
   };
 
   MovieWindow():
-      controls(this), buttons(false, 0), render("Render"),
-      cancel(Gtk::Stock::CANCEL), vbox(false, 0), working(false) {
-    controls.UpdateDisplay();
-    frame.add(controls);
+      buttons(false, 0), render("Render"), cancel(Gtk::Stock::CANCEL),
+      vbox(false, 0), working(false) {}
+
+  ~MovieWindow() {}
+
+  void Initialize() {
+    controls = new MovieControls(this);
+    controls->UpdateDisplay();
+    frame.add(*controls);
     // Action buttons & progress report
     render.signal_clicked().connect(sigc::mem_fun(*this, &MovieWindow::Render));
     buttons.pack_start(render, false, false, 1);
@@ -132,13 +115,15 @@ public:
     vbox.pack_start(frame, false, false, 1);
     vbox.pack_end(buttons, true, true, 0);
     add(vbox);
+    controls->UpdateDisplay();
+    show_all();
   }
 
-  ~MovieWindow() {}
-
   bool configurationValid() {
-    bool valid = controls.allDisplaysValid();
-    if(controls.m_path.size() == 0 || controls.m_ffmpeg.size() == 0)
+    if(!controls)
+      return false;
+    bool valid = controls->allDisplaysValid();
+    if(m_path.size() == 0 || m_ffmpeg.size() == 0)
       valid = false;
     return valid;
   }
@@ -151,8 +136,8 @@ public:
   void Render() {
     assert(configurationValid());
     render.set_sensitive(false);
-    controls.ContainerActivated(); // TODO a misnamed now?
-    controls.SetSensitivity(false);
+    controls->ContainerActivated(); // TODO a misnamed now?
+    controls->SetSensitivity(false);
     working = true;
     ThreadCreate(thread, MovieWindow::worker, this);
   }
@@ -167,22 +152,22 @@ public:
   // Worker thread that renders the movie
   void worker() {
     RenderMovieThread rmt(this);
-    rmt.sx = controls.m_x;
-    rmt.sy = controls.m_y;
+    rmt.sx = m_x;
+    rmt.sy = m_y;
     rmt.sr = 2;
-    rmt.ex = controls.m_x;
-    rmt.ey = controls.m_y;
-    rmt.er = controls.m_radius;
-    rmt.maxiters = controls.m_maxiters;
-    rmt.width = 1280;
-    rmt.height = 720; // TODO configurable
-    rmt.seconds = controls.m_seconds;
-    rmt.fps = controls.m_fps;
-    rmt.bitrate = controls.m_bitrate;
-    rmt.arith = controls.m_arith;
-    rmt.ffmpeg = controls.m_ffmpeg;
-    rmt.codec = controls.m_codec;
-    rmt.path = controls.m_path;
+    rmt.ex = m_x;
+    rmt.ey = m_y;
+    rmt.er = m_radius;
+    rmt.maxiters = m_maxiters;
+    rmt.width = m_width;
+    rmt.height = m_height;
+    rmt.seconds = m_seconds;
+    rmt.fps = m_fps;
+    rmt.bitrate = m_bitrate;
+    rmt.arith = m_arith;
+    rmt.ffmpeg = m_ffmpeg;
+    rmt.codec = m_codec;
+    rmt.path = m_path;
     rmt.Render();
   }
 
@@ -206,22 +191,64 @@ public:
   }
 };
 
+MovieControls::MovieControls(MovieWindow *window):
+    m_window(window),
+    m_x_control(this, &m_window->m_x, -arith_traits<arith_t>::maximum(),
+                arith_traits<arith_t>::maximum()),
+    m_y_control(this, &m_window->m_y, -arith_traits<arith_t>::maximum(),
+                arith_traits<arith_t>::maximum()),
+    m_radius_control(this, &m_window->m_radius, 0,
+                     arith_traits<arith_t>::maximum()),
+    m_maxiters_control(this, &m_window->m_maxiters, 1, INT_MAX - 1),
+    m_seconds_control(this, &m_window->m_seconds, 1, INT_MAX - 1),
+    m_fps_control(this, &m_window->m_fps, 1, INT_MAX - 1),
+    m_bitrate_control(this, &m_window->m_bitrate, 1, INT_MAX - 1),
+    m_width_control(this, &m_window->m_width, 32, 15360),
+    m_height_control(this, &m_window->m_height, 32, 8640),
+    m_codec_control(this, &m_window->m_codec),
+    m_ffmpeg_control(this, &m_window->m_ffmpeg, "Select Encoder"),
+    m_path_control(this, &m_window->m_path),
+    m_arith_control(this, &m_window->m_arith_string, &arith_names[0],
+                    &arith_names[arith_limit]) {
+
+  m_x_control.Attach(0, 0, "X center");
+  m_y_control.Attach(0, 1, "Y center");
+  m_radius_control.Attach(0, 2, "Radius");
+  m_maxiters_control.Attach(0, 3, "Iterations");
+  m_width_control.Attach(0, 4, "Width");
+
+  m_seconds_control.Attach(1, 0, "Length (s)");
+  m_fps_control.Attach(1, 1, "Frames/s");
+  m_bitrate_control.Attach(1, 2, "Bits/s");
+  m_codec_control.Attach(1, 3, "Codec");
+  m_height_control.Attach(1, 4, "Height");
+  m_arith_control.Attach(1, 5, "Precision");
+
+  m_ffmpeg_control.Attach(0, 6, "Encoder", 2);
+  m_path_control.Attach(0, 7, "Filename", 2);
+}
+
 void MovieControls::controlChanged(Control *c) {
   m_window->controlChanged();
   if(c == &m_ffmpeg_control) {
     c->UpdateUnderlying();
+  } else if(c == &m_arith_control) {
+    c->UpdateUnderlying();
+    arith_type new_arith = string_to_arith(m_window->m_arith_string);
+    if(new_arith != m_window->m_arith)
+      m_window->m_arith = new_arith;
   }
 }
 
 void Movie(arith_t x, arith_t y, arith_t radius, int maxiters,
            arith_type arith) {
   MovieWindow *w = new MovieWindow();
-  w->controls.m_x = x;
-  w->controls.m_y = y;
-  w->controls.m_radius = radius;
-  w->controls.m_maxiters = maxiters;
-  w->controls.m_arith = arith;
-  w->controls.UpdateDisplay();
-  w->show_all();
+  w->m_x = x;
+  w->m_y = y;
+  w->m_radius = radius;
+  w->m_maxiters = maxiters;
+  w->m_arith = arith;
+  w->m_arith_string = arith_names[arith];
+  w->Initialize();
 }
 }; // namespace mmui
