f82c49e610105c2145211105837a1aca327419d0
Richard Kettlewell <rjk@greenend.org.uk>
2021-11-27 18:48:06 +0000

    remove a stray copyright date

--- gtkui/MainMenu.cc
+++ gtkui/MainMenu.cc
@@ -142,8 +142,7 @@ public:
     Gtk::Label name;
     name.set_markup("<span font_desc=\"Sans 36\">Mandy</span>");
     Gtk::Label description("Mandelbrot/Julia Set Generator");
-    Gtk::Label copyright("Version " VERSION
-                         " \xC2\xA9 2010 Richard Kettlewell");
+    Gtk::Label copyright("Version " VERSION " \xC2\xA9 Richard Kettlewell");
     about.get_vbox()->pack_start(name, Gtk::PACK_SHRINK);
     about.get_vbox()->pack_start(description, Gtk::PACK_SHRINK);
     Glib::RefPtr<Gdk::Pixbuf> logo_pixbuf =
