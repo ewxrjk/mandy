8cbddb7bc6ca3235f63712cb64a186529a4bda36
Richard Kettlewell <rjk@terraraq.uk>
2024-04-26 16:31:57 +0100

    ARM fixes

--- lib/cycletest.cc
+++ lib/cycletest.cc
@@ -15,9 +15,26 @@
  */
 #include "mandy.h"
 #include "arith.h"
+
 #if __amd64__
 # include <x86intrin.h>
+# define CYCLES() __rdtsc()
 #endif
+
+#if __aarch64__ && __clang__
+# include <arm_acle.h>
+#define CYCLES() __arm_rsr64("CNTVCT_EL0")
+#endif
+
+#if __aarch64__ && ! defined CYCLES
+static inline unsigned long long cycles(void) {
+  unsigned long long r;
+  __asm__ volatile("mrs %0,CNTVCT_EL0" : "=r"(r));
+  return r;
+}
+#define CYCLES() cycles()
+#endif
+
 #if SIMD
 #include "simdarith.h"
 #endif
@@ -30,10 +47,10 @@ int main(int argc, char **argv) {
   double zxvalues[SIMD_MAX] = {0}, zyvalues[SIMD_MAX] = {0}, cxvalues[SIMD_MAX] = {0}, cyvalues[SIMD_MAX] = {0},
          r2values[SIMD_MAX] = {0};
   int iterations[SIMD_MAX];
-  unsigned long long start = __rdtsc();
+  unsigned long long start = CYCLES();
   simd_iterate(zxvalues, zyvalues, cxvalues, cyvalues, maxiter, iterations, r2values, 0);
   __asm__ volatile("" : : "m"(iterations[0]), "m"(r2values[0]), "m"(iterations[2]), "m"(r2values[2]));
-  unsigned long long finish = __rdtsc();
+  unsigned long long finish = CYCLES();
   unsigned long long cycles = finish - start;
   printf("iterations %d/%d cycles %llu cycles/iteration %f\n",
          iterations[0],
